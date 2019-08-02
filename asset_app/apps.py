bb732281416c56e9ae3ed45dbd620e514521474f
jameskomo <james.komoh@gmail.com>
2019-08-02 13:38:40 +0300

    Initial project repository created, Django installed and django app and project created, app set up in settings.py and LICENSE created

---
+++ asset_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AssetAppConfig(AppConfig):
+    name = 'asset_app'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/komo/Documents/moringa-school/Python/Django/asset-management-system/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
